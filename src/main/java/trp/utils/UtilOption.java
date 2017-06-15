eaa8c897518fab6d630258edfa870a15cde8f85f
junhomax <ckwnsgh1991@gmail.com>
2017-06-15 15:42:07 +0900

    Modifying UTIL_OPTION Constans

--- src/main/java/trp/utils/UtilOption.java
+++ src/main/java/trp/utils/UtilOption.java
@@ -7,5 +7,5 @@ public class UtilOption {
     public static String UTF8               = "UTF-8";
     public static int FILE_IO_BUFFER_SIZE   = 1024 * 8;
     public static String LINE_SEPARATOR     = "line.separator";
-    public static String RESOURCE_PATH      = "src/main/resources/serializable/spacing/";
+    public static String RESOURCE_PATH      = "src/main/resources/";
 }
